65c467e74a6fa11019db931e31d3f2282cc590b2
bhupiiidx <bhupender81238@gmail.com>
2024-02-23 18:07:02 +0530

    timer added

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const root = document.getElementById("root");
-let newArr = [...Array(30)].map((x, i) => i);
-let puzzeledArray = [];
-for (let i = 0; i < 30; i++) {
-	let index = Math.floor(Math.random() * newArr.length);
-	puzzeledArray.push(newArr[index]);
-	newArr.splice(index, 1);
-}
-
-let rootLitral = "";
-for (let i = 0; i < 5; i++) {
-	rootLitral += `<ul class="list-group">`;
-	for (let j = i * 5; j < i * 5 + 5; j++) {
-		rootLitral += `<li class="list-group-item">${puzzeledArray[j]}</li>`;
+const showPuzzle = () => {
+	let newArr = [...Array(30)].map((x, i) => i);
+	let puzzeledArray = [];
+	for (let i = 0; i < 30; i++) {
+		let index = Math.floor(Math.random() * newArr.length);
+		puzzeledArray.push(newArr[index]);
+		newArr.splice(index, 1);
 	}
-	rootLitral += `</ul>`;
-}
+	let rootLitral = "";
+	for (let i = 0; i < 5; i++) {
+		rootLitral += `<ul class="list-group">`;
+		for (let j = i * 5; j < i * 5 + 5; j++) {
+			rootLitral += `<li class="list-group-item">${puzzeledArray[j]}</li>`;
+		}
+		rootLitral += `</ul>`;
+	}
+	$("#rootPuzzle").html(rootLitral);
+	rootPuzzle.innerHTML = rootLitral;
+};
+const startTimer = () => {
+	let seconds = 30;
 
-root.innerHTML = rootLitral;
+	// Update the countdown every 1 second
+	const timer = setInterval(function () {
+		$("#rootTimer").removeClass("text-danger");
+		$("#rootTimer").addClass("text-success");
+		$("#rootTimer").html("0:" + seconds + "s");
+		seconds--;
 
-console.log("puzzeledArray", puzzeledArray);
+		// If the countdown reaches 0, stop the timer
+		if (seconds < 0) {
+			clearInterval(timer);
+			$("#rootTimer").html("Time's up!");
+			$("#rootTimer").addClass("text-danger");
+		}
+	}, 1000);
+};
+
+const startPuzzle = () => {
+	$("#start").addClass("d-none");
+	$("#stop").removeClass("d-none");
+	showPuzzle();
+	startTimer();
+	$("#wrapper").removeClass("d-none");
+};
+const stopPuzzle = () => {
+	$("#stop").addClass("d-none");
+	$("#start").removeClass("d-none");
+	$("#wrapper").addClass("d-none");
+};
